7dd8739d109a08a1e7f7f1502e1b2fb38b43b0ef
yunzhan2014 <yunzhan2016@gmail.com>
2017-06-05 10:36:04 +0800

    增加topK的计算与打印输出

--- cf.py
+++ cf.py
@@ -1,46 +1,82 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-header = ['user_id', 'item_id', 'rating', 'timestamp']
-df = pd.read_csv('../ml-100k/u.data', sep='\t', names=header)
-
+topk_value = 10
+np.seterr(divide='ignore', invalid='ignore')
+header = ['user_id', 'item_id', 'rating']
+# df = pd.read_csv('/home/elics-lee/acdamicSpace/dataset/ml-100k/u.data', sep='\t', names=header)
+df = pd.read_csv('/home/elics-lee/opt/node2vec/graph/FilmTrust/ratings.txt', sep=' ', names=header)
 n_users = df.user_id.unique().shape[0]
 n_items = df.item_id.unique().shape[0]
 print('Number of users = ' + str(n_users) + ' | Number of movies = ' + str(n_items))
 
-from sklearn import cross_validation as cv
-train_data, test_data = cv.train_test_split(df, test_size=0.25)
+train_data, test_data = train_test_split(df, test_size=0.2)
 
-#Create two user-item matrices, one for training and another for testing
+# Create two user-item matrices, one for training and another for testing
 train_data_matrix = np.zeros((n_users, n_items))
 for line in train_data.itertuples():
-        train_data_matrix[line[1]-1, line[2]-1] = line[3]
+    train_data_matrix[line[1] - 1, line[2] - 1] = line[3]
 
 test_data_matrix = np.zeros((n_users, n_items))
 for line in test_data.itertuples():
-        test_data_matrix[line[1]-1, line[2]-1] = line[3]
+    test_data_matrix[line[1] - 1, line[2] - 1] = line[3]
 
-from sklearn.metrics.pairwise import pairwise_distances
-user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
-item_similarity = pairwise_distances(train_data_matrix.T, metric='cosine')
+user_similarity = cosine_similarity(train_data_matrix)
+item_similarity = cosine_similarity(train_data_matrix.T)
 
-def predict(ratings, similarity, type='user'):
+x, y = train_data_matrix.nonzero()
+
+
+def rating_predict(ratings, similarity, type='user'):
+    temp = ratings.copy()
+    for i, j in zip(x, y):
+        temp[i][j] = 1
+
     if type == 'user':
-        mean_user_rating = ratings.mean(axis=1)
-        #You use np.newaxis so that mean_user_rating has same format as ratings
-        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
-        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
-
-    elif type == 'item':
-        pred = ratings.dot(similarity) / np.nan_to_num(ratings/ratings).dot(similarity)
+        pred = similarity.dot(ratings) / similarity.dot(temp)
         pred = np.nan_to_num(pred)
+    elif type == 'item':
+        pred = ratings.dot(similarity) / temp.dot(similarity)
+        pred = np.nan_to_num(pred)
+
+    for i, j in zip(x, y):
+        pred[i][j] = 0
     return pred
 
-item_prediction = predict(train_data_matrix, item_similarity, type='item')
-user_prediction = predict(train_data_matrix, user_similarity, type='user')
 
-from sklearn.metrics import mean_squared_error
-from math import sqrt
+user_prediction = rating_predict(train_data_matrix, user_similarity, type='user')
+item_prediction = rating_predict(train_data_matrix, item_similarity, type='item')
+
+
+def top_k_list(prediction_matrix, k_value):
+    index_num = 1
+    topk_list = dict()
+    for row in prediction_matrix:
+        basket = row.argsort()[::-1][0:k_value]
+        topk_list.setdefault(index_num, list(basket))
+        index_num += 1
+    return topk_list
+
+
+def construct_dict(data):
+    user_dict = {}
+    item_dict = {}
+    for row in data.itertuples():
+        user_dict.setdefault(row[1], []).append(row[2])
+        item_dict.setdefault(row[2], []).append(row[1])
+    return user_dict, item_dict
+
+
+def filter_rating(data_matrix, data_dict):
+    filter_matrix = data_matrix.copy()
+    for key, value in data_dict.items():
+        filter_matrix.loc[key, value] = 0
+    return filter_matrix
+
 
 def rmse(prediction, ground_truth):
     prediction = prediction[ground_truth.nonzero()].flatten()
@@ -48,15 +84,46 @@ def rmse(prediction, ground_truth):
     return sqrt(mean_squared_error(prediction, ground_truth))
 
 
+def evaluation(topk_dict, test_dict, k_val, test_dict_num):
+    relevant_num = 0
+    topk_dict_num = len(topk_dict) * k_val
+
+    for key, value in topk_dict.items():
+        # rank = 0
+        if key in test_dict:
+            overlap_set = set(value).intersection(test_dict[key])
+            row_overlap_num = len(overlap_set)
+            relevant_num += row_overlap_num
+            # for i in overlap_set:
+            #    rank += ((i/value.index(i)) for i in overlap_set)
+
+    print('relevant num:' + str(relevant_num))
+    print('topk_dict_num:' + str(topk_dict_num))
+    print('test_dict_num:' + str(test_dict_num))
+
+    precision = relevant_num / topk_dict_num
+    recall = relevant_num / test_dict_num
+    return precision, recall
+
+
 print('User-based CF RMSE: ' + str(rmse(user_prediction, test_data_matrix)))
 print('Item-based CF RMSE: ' + str(rmse(item_prediction, test_data_matrix)))
 
+item_top_list = top_k_list(item_prediction, topk_value)
+user_top_list = top_k_list(user_prediction, topk_value)
+user_dict_test, item_dict_test = construct_dict(test_data)
+user_precision, user_recall = evaluation(item_top_list, user_dict_test, topk_value, len(test_data))
+item_precision, item_recall = evaluation(item_top_list, item_dict_test, topk_value, len(test_data))
 
-import scipy.sparse as sp
-from scipy.sparse.linalg import svds
-
-#get SVD components from train matrix. Choose k.
-u, s, vt = svds(train_data_matrix, k = 20)
-s_diag_matrix=np.diag(s)
-X_pred = np.dot(np.dot(u, s_diag_matrix), vt)
-print('User-based CF MSE: ' + str(rmse(X_pred, test_data_matrix)))
+print('User-based CF Precision: %.5f' %(user_precision))
+print('User-based CF recall: %.5f' %(user_recall))
+print('Item-based CF Precision: %.5f' %(item_precision))
+print('Item-based CF recall: %.5f' %(item_recall))
+# import scipy.sparse as sp
+# from scipy.sparse.linalg import svds
+#
+# get SVD components from train matrix. Choose k.
+# u, s, vt = svds(train_data_matrix, k=20)
+# s_diag_matrix = np.diag(s)
+# X_pred = np.dot(np.dot(u, s_diag_matrix), vt)
+# print('User-based CF MSE: ' + str(rmse(X_pred, test_data_matrix)))
--- node2vecOp.py
+++ node2vecOp.py
@@ -3,10 +3,10 @@ import numpy as np
 from sklearn.metrics import pairwise
 from sklearn.model_selection import train_test_split
 
-user_num = 0
-
-
+#dataset_file_path = '/home/elics-lee/acdamicSpace/dataset/ml-1m/ratings.dat'
+dataset_file_path = 'graph/FilmTrust/ratings.txt'
 def cv_data(source_data, rate=0.2):
+    # header = ['userId', 'itemId', 'ratings','time']
     header = ['userId', 'itemId', 'ratings']
     data = pd.read_csv(source_data, sep=' ', header=None, names=header)
     user_num = data.userId.unique().shape[0]
@@ -14,13 +14,14 @@ def cv_data(source_data, rate=0.2):
     train_data, test_data = train_test_split(data, test_size=rate)
     return train_data, test_data
 
-
 def load_DataFrame(file):
     with open(file) as f:
         table = pd.read_table(f, sep=' ', header=None, index_col=0, names=None, lineterminator='\n')
-    print(table.head())
     table = table.sort_index(axis=0)
     label_index = np.asarray(table.index)
+    #table = table.abs()
+    table = (table - table.min()) / (table.max() - table.min())
+    #table = (table - table.mean()) / table.std()
     s = np.asarray(table)
     sim_matrix = pairwise.cosine_similarity(s)
     sim_matrix = pd.DataFrame(sim_matrix, index=label_index, columns=label_index)
@@ -55,18 +56,15 @@ def evaluation(topk_dict, test_dict, k_val, test_dict_num):
     topk_dict_num = len(topk_dict) * k_val
 
     for key, value in topk_dict.items():
-        rank = 0
+        #rank = 0
         if key in test_dict:
-            print(key)
             overlap_set = set(value).intersection(test_dict[key])
             row_overlap_num = len(overlap_set)
             relevant_num += row_overlap_num
-            for i in overlap_set:
-                rank += ((i/value.index(i)) for i in overlap_set)
+            #for i in overlap_set:
+            #    rank += ((i/value.index(i)) for i in overlap_set)
                     
                     
-                    
-
     print('relevant num:' + str(relevant_num))
     print('topk_dict_num:' + str(topk_dict_num))
     print('test_dict_num:' + str(test_dict_num))
@@ -81,12 +79,40 @@ def filter_rating(data_matrix,data_dict):
         filter_matrix.loc[[key],value] = 0
     return filter_matrix
 
-def excute(file, test_data, train_data):
-    sim_matrix, label_index = load_DataFrame(file, 0.2)
-    item_matrix, user_matrix = matrix_split(sim_matrix, 1508)
-    user_dict_train, item_dict_train = construct_dict(train_data)
-    user_dict_test, item_dict_train = construct_dict(test_data)
-    top_list = [10, 30, 50]
-    topK_dict = dict()
-    for k in top_list:
-        topK_dict.setdefault(k, {}).append(top_k_list(user_matrix, k))
+##
+header = ['userId', 'itemId', 'ratings']
+train_data = pd.read_csv('/home/elics-lee/acdamicSpace/dataset/ml-1m/graph/train.csv', sep=' ',names=header)
+test_data = pd.read_csv('/home/elics-lee/acdamicSpace/dataset/ml-1m/graph/test.csv', sep=' ', names=header)
+topk_value = 30
+user_num = 6040
+
+emb_file = '/home/elics-lee/acdamicSpace/dataset/ml-1m/emb/ml.emb'
+
+#emb_file = 'emb/filmtrustK10WalkL30.emb'
+#emb_file = 'emb/filmtrustK10WalkL120.emb'
+#emb_file = 'emb/filmtrustK10WalkL160.emb'
+
+#emb_file = 'emb/filmtrustK10.emb'
+#emb_file = 'emb/filmtrustK30.emb'
+#emb_file = 'emb/filmtrustK60.emb'
+#emb_file = 'emb/filmtrustK128.emb'
+#emb_file = 'emb/filmtrustK15.emb'
+#emb_file = 'emb/filmtrustK8.emb'
+sim_matrix, label_index = load_DataFrame(emb_file)
+item_matrix, user_matrix = matrix_split(sim_matrix, user_num)
+user_dict_train, item_dict_train = construct_dict(train_data)
+user_dict_test, item_dict_test = construct_dict(test_data)
+
+filter_user_matrix = filter_rating(user_matrix,user_dict_train)
+filter_item_matrix = filter_rating(item_matrix,item_dict_train)
+
+user_top_list = top_k_list(filter_user_matrix,topk_value)
+item_top_list = top_k_list(filter_item_matrix,topk_value)
+
+user_precision, user_recall = evaluation(user_top_list,user_dict_test,topk_value,len(test_data))
+item_precision, item_recall = evaluation(item_top_list,item_dict_test,topk_value,len(test_data))
+
+print('User-based embeding Precision: %.5f' %(user_precision))
+print('User-based embeding recall: %.5f' %(user_recall))      
+print('Item-based embeding Precision: %.5f' %(item_precision))
+print('Item-based embeding recall: %.5f' %(item_recall))      
