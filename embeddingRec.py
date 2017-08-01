bb976b18e57d838d0536597967bf21a3184f3876
yunzhan2014 <yunzhan2016@gmail.com>
2017-08-01 23:32:02 +0800

    review了kd tree的部分，修改了之前逻辑不正确的地方

--- embeddingRec.py
+++ embeddingRec.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KDTree
 from sklearn.metrics import pairwise
+from os import listdir
 
 
 class Node2vec(object):
@@ -11,7 +12,7 @@ class Node2vec(object):
     embedding_file等一系列的属性信息
     """
     header = ['userId', 'itemId', 'ratings']
-    data_set_path = '/home/elics-lee/academicSpace/dataSet/ciao'
+    data_set_path = '/home/elics-lee/academicSpace/dataSet/FilmTrust'
 
     def __init__(self, embedding_file, top_k_value):
         """
@@ -40,40 +41,36 @@ def base_cosine_similarity_list(embed_entity):
     train_data = embed_entity.train_data
     test_data = embed_entity.test_data
     top_k_value = embed_entity.top_k_value
-    emb_file_list = ["embl_1.txt", "embl_2.txt", "embl_3.txt", "embl_4.txt", "embl_5.txt", "embl_6.txt", "embl_7.txt",
-                     "embl_8.txt", "embl_9.txt"]
-    for i in emb_file_list:
-        emb_file = '%s/emb/' % data_set_path
-        emb_file = emb_file + str(i)
-        print(emb_file)
-        sim_matrix, label_index = cosine_similarity(emb_file)
-        item_matrix, user_matrix = matrix_split(sim_matrix, user_num)
-        # 分别得到基于用户和基于物品的测试集合
-        user_dict_train, item_dict_train = construct_dict(train_data)
-        user_dict_test, item_dict_test = construct_dict(test_data)
-        # 分别对用户-物品和物品-用户的相似度矩阵剔除train data当中集合
-        filter_user_matrix = filter_rating(user_matrix, user_dict_train)
-        filter_item_matrix = filter_rating(item_matrix, item_dict_train)
-        # 分别得到基于用户和项目的物品的topK推荐列表
-        user_top_list = top_k_list(filter_user_matrix, top_k_value)
-        item_top_list = top_k_list(filter_item_matrix, top_k_value)
-        # 分别得到基于用户和项目的物品的topK推荐列表的评价指标
-        user_result = evaluation(user_top_list, user_dict_test, top_k_value)
-        item_result = evaluation(item_top_list, item_dict_test, top_k_value)
-        # 把评估的结果写入到result文件当中
-        result_file = "%s/result/result.csv" % data_set_path
-        result_list = list()
-        result_list.append(i)
-        result_list.append(str(top_k_value))
-        result_list.append("{:06.5f}".format(user_result['precision']))
-        result_list.append("{:06.5f}".format(user_result['recall']))
-        result_list.append("{:06.5f}".format(user_result['ncrr']))
-        result_list.append("{:06.5f}".format(item_result['precision']))
-        result_list.append("{:06.5f}".format(item_result['recall']))
-        result_list.append("{:06.5f}".format(item_result['ncrr']))
-        with open(result_file, 'a') as f:
-            f.write(','.join(result_list) + "\n")
-        f.close()
+    emb_file = embed_entity.emb_file
+    print(emb_file)
+    sim_matrix, label_index = cosine_similarity(emb_file)
+    item_matrix, user_matrix = matrix_split(sim_matrix, user_num)
+    # 分别得到基于用户和基于物品的测试集合
+    user_dict_train, item_dict_train = construct_dict(train_data)
+    user_dict_test, item_dict_test = construct_dict(test_data)
+    # 分别对用户-物品和物品-用户的相似度矩阵剔除train data当中集合
+    filter_user_matrix = filter_rating(user_matrix, user_dict_train)
+    filter_item_matrix = filter_rating(item_matrix, item_dict_train)
+    # 分别得到基于用户和项目的物品的topK推荐列表
+    user_top_list = top_k_list(filter_user_matrix, top_k_value)
+    item_top_list = top_k_list(filter_item_matrix, top_k_value)
+    # 分别得到基于用户和项目的物品的topK推荐列表的评价指标
+    user_result = evaluation(user_top_list, user_dict_test, top_k_value)
+    item_result = evaluation(item_top_list, item_dict_test, top_k_value)
+    # 把评估的结果写入到result文件当中
+    result_file = "%s/result/result.csv" % data_set_path
+    result_list = list()
+    result_list.append("cosine")
+    result_list.append(str(top_k_value))
+    result_list.append("{:06.5f}".format(user_result['precision']))
+    result_list.append("{:06.5f}".format(user_result['recall']))
+    result_list.append("{:06.5f}".format(user_result['ncrr']))
+    result_list.append("{:06.5f}".format(item_result['precision']))
+    result_list.append("{:06.5f}".format(item_result['recall']))
+    result_list.append("{:06.5f}".format(item_result['ncrr']))
+    with open(result_file, 'a') as f:
+        f.write(','.join(result_list) + "\n")
+    f.close()
 
 
 def base_kd_tree_similarity_list(embed_entity):
@@ -89,20 +86,21 @@ def base_kd_tree_similarity_list(embed_entity):
     emb_file = embed_entity.emb_file
     user_num = embed_entity.user_num
     top_k_value = embed_entity.top_k_value
+    print("这一组的top k value 是：%d" % top_k_value)
     # 分别得到基于用户和基于物品的训练集合与测试集合
     user_dict_train, item_dict_train = construct_dict(train_data)
     user_dict_test, item_dict_test = construct_dict(test_data)
     # 根据最近生成top list
-    top_list = kd_tree_similarity(emb_file, user_num, top_k_value)
+    user_top_list, item_top_list = kd_tree_similarity(emb_file, user_num)
     # 把top list分割成user:item list和item:user list的形式
-    keys = list(top_list.keys())
-    user_keys = [i for i in keys if i <= user_num]
-    item_keys = [i for i in keys if i > user_num]
-    user_top_list = {k: top_list[k] for k in user_keys}
-    item_top_list = {k: top_list[k] for k in item_keys}
+    # keys = list(top_list.keys())
+    # user_keys = [i for i in keys if i <= user_num]
+    # item_keys = [i for i in keys if i > user_num]
+    # user_top_list = {k: top_list[k] for k in user_keys}
+    # item_top_list = {k: top_list[k] for k in item_keys}
     # 过滤掉train data中已经包含的评分信息(目前会导致top list变小)
-    user_top_list = filter_kd_tree(user_top_list, user_dict_train)
-    item_top_list = filter_kd_tree(item_top_list, item_dict_train)
+    user_top_list = filter_kd_tree(user_top_list, user_dict_train, top_k_value)
+    item_top_list = filter_kd_tree(item_top_list, item_dict_train, top_k_value)
     # 分别得到基于用户和项目的物品的topK推荐列表的评价指标
     user_result = evaluation(user_top_list, user_dict_test, top_k_value)
     item_result = evaluation(item_top_list, item_dict_test, top_k_value)
@@ -122,49 +120,62 @@ def base_kd_tree_similarity_list(embed_entity):
     f.close()
 
 
-def kd_tree_similarity(file, user_numbers, top_k_value, distance='euclidean'):
+def kd_tree_similarity(file, user_numbers, distance='euclidean'):
     """
     使用kd tree寻找最近邻"
     :param file:
     :param user_numbers: 作为kd tree的leaf_size
-    :param top_k_value:
     :param distance: 选择kd tree所采用的distance类型
-    :return:
+    :return: dict: id号对应的一系列id
     """
     with open(file) as f:
         table = pd.read_table(f, sep=' ', header=None, index_col=0, names=None, lineterminator='\n')
     table = table.sort_index(axis=0)
+    # normalize row
     table = table.div(np.sqrt(table.pow(2).sum()), axis=0)
     table = table.fillna(0)
+    # split table to user table and item table
     label_index = np.asarray(table.index)
-    tree = KDTree(table, metric=distance, leaf_size=user_numbers)
-    result = dict()
-    for i in label_index:
-        # q_list = table.loc[i].reshape(1, -1)
-        q_list = np.reshape(table.loc[i], (1, -1))
-        dist, ind = tree.query(q_list, k=top_k_value * 35)
-        result[i] = [label_index[j] for j in ind[0]]
-    return result
+    user_label_index = label_index[label_index <= user_numbers]
+    item_label_index = label_index[label_index > user_numbers]
+    # 根据user label index筛选出user table
+    user_table = table.ix[user_label_index]
+    item_table = table.ix[item_label_index]
+    # 基于用户挑选的最近邻列表
+    tree = KDTree(item_table, metric=distance, leaf_size=user_numbers)
+    user_result = dict()
+    for i in user_table.iterrows():
+        user_i = list()
+        user_i.append(i[1].tolist())
+        dist, ind = tree.query(user_i, k=100)
+        user_result[i[0]] = [item_label_index[j] for j in ind[0]]
+    # 基于物品挑选的最近邻列表
+    tree = KDTree(user_table, metric=distance, leaf_size=user_numbers)
+    item_result = dict()
+    for i in item_table.iterrows():
+        item_i = list()
+        item_i.append(i[1].tolist())
+        dist, ind = tree.query(item_i, k=100)
+        item_result[i[0]] = [user_label_index[j] for j in ind[0]]
+    return user_result, item_result
 
 
-def cosine(x, y):
-    return pairwise.cosine_distances(x, y)
-
-
-def filter_kd_tree(neighbor_list, data_dict):
+def filter_kd_tree(top_dict, train_dict, top_k_value):
     """
     对采用kd tree方式的top list进行训练集的筛选和过滤
-    :param neighbor_list:
-    :param data_dict:
-    :return:
+    :param top_dict:
+    :param train_dict:
+    :param top_k_value
+    :return: dict
     """
     result = dict()
-    for key, value in data_dict.items():
-        if key in neighbor_list:
-            row_list = neighbor_list[key]
+    for key, value in train_dict.items():
+        if key in top_dict:
+            row_list = top_dict[key]
             list_intersection = set(value).intersection(row_list)
-            l = [x for x in value if x not in list_intersection]
-            result[key] = l
+            l = [x for x in row_list if x not in list_intersection]
+            result[key] = l[0:top_k_value]
+            print("top %d in user or item %d" % (len(value), key))
     return result
 
 
@@ -281,9 +292,14 @@ def evaluation(top_dict, test_dict, k_val):
 
 
 def main():
-    node2vec = Node2vec("embl_1.txt", 40)
+    data_set_path = '/home/elics-lee/academicSpace/dataSet/FilmTrust/emb/'
+    embedding_list = listdir(data_set_path)
+    counter = 0
+    for embedding_file in embedding_list:
+        print("This is %d loop, embedding file is %s" %(counter, embedding_file))
+        node2vec = Node2vec(embedding_file, 10)
     # base_cosine_similarity_list(node2vec)
-    base_kd_tree_similarity_list(node2vec)
+        base_kd_tree_similarity_list(node2vec)
 
 
 if __name__ == "__main__":
